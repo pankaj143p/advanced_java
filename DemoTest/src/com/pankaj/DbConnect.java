df547d004742694bea6948a833883ff2f7953261
Pankaj Prajapati <114794372+pankaj143p@users.noreply.github.com>
2024-12-02 10:34:57 +0530

    crud operation by using preparedStatement

--- DemoTest/src/com/pankaj/DbConnect.java
+++ DemoTest/src/com/pankaj/DbConnect.java
@@ -2,6 +2,7 @@ package com.pankaj;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -24,8 +25,19 @@ public class DbConnect {
 	        Statement statement = ((java.sql.Connection) connection).createStatement();	
 //	        for update 
 //	        String query = String.format("UPDATE teacher SET marks = %f WHERE id = %d" , 87.40,2);
-	        String query = String.format("DELETE from teacher WHERE ID = 2");
-	        int row = statement.executeUpdate(query);
+//	        for delete 
+//	        String query = String.format("DELETE from teacher WHERE ID = 2");
+//	        int row = statement.executeUpdate(query);
+	        
+	        
+//	         CRUD operations using preparedStatement 
+	        
+	        String query = "INSERT INTO teacher(name, age, marks) VALUES(?, ?, ?)";
+	        PreparedStatement ps = ((java.sql.Connection) connection).prepareStatement(query);
+	        ps.setString(1, "Anshika");
+	        ps.setInt(2, 21);
+	        ps.setDouble(3, 93.45);
+	        int row = ps.executeUpdate();
 	        if(row>0) {
 	        	System.out.println("delete successfully");
 	        }else {
