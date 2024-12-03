01bcd4f194961cb6ec73742b55053e7ab2ea6b41
Pankaj Prajapati <114794372+pankaj143p@users.noreply.github.com>
2024-12-03 14:06:32 +0530

    by user input

--- DemoTest/src/com/pankaj/DbConnect.java
+++ DemoTest/src/com/pankaj/DbConnect.java
@@ -6,12 +6,16 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Scanner;
 
 public class DbConnect {
      private static final String url = "jdbc:mysql://127.0.0.1:3306/combinedb";
      private static final String username = "root";
      private static final String password = "123@Code";
-	 public static void main(String[] args) {
+	 /**
+	 * @param args
+	 */
+	public static void main(String[] args) {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 		} catch (ClassNotFoundException e) {
@@ -22,27 +26,9 @@ public class DbConnect {
 		try {
 	
 			Connection connection = (Connection) DriverManager.getConnection(url,username, password);
-	        Statement statement = ((java.sql.Connection) connection).createStatement();	
-//	        for update 
-//	        String query = String.format("UPDATE teacher SET marks = %f WHERE id = %d" , 87.40,2);
-//	        for delete 
-//	        String query = String.format("DELETE from teacher WHERE ID = 2");
-//	        int row = statement.executeUpdate(query);
-	        
-	        
-//	         CRUD operations using preparedStatement 
-	        
-	        String query = "INSERT INTO teacher(name, age, marks) VALUES(?, ?, ?)";
-	        PreparedStatement ps = ((java.sql.Connection) connection).prepareStatement(query);
-	        ps.setString(1, "Anshika");
-	        ps.setInt(2, 21);
-	        ps.setDouble(3, 93.45);
-	        int row = ps.executeUpdate();
-	        if(row>0) {
-	        	System.out.println("delete successfully");
-	        }else {
-	        	System.out.println("any error");
-	        }
+//	        Statement statement = ((java.sql.Connection) connection).createStatement();	
+			
+//			<-- print only-->
 //	        String query = "select * from teacher";
 //	        ResultSet res = statement.executeQuery(query);
 //	        while(res.next()) {
@@ -55,6 +41,86 @@ public class DbConnect {
 //	        	System.out.println("hey my age "+age);
 //	        	System.out.println("hey my is marks "+marks);
 //	        }
+			
+			
+			
+//	        <-- for update -->
+//	        String query = String.format("UPDATE teacher SET marks = %f WHERE id = %d" , 87.40,2);
+			
+//	        <-- for delete --> 
+//	        String query = String.format("DELETE from teacher WHERE ID = 2");
+//	        int row = statement.executeUpdate(query);
+	        
+	        
+//	        <-------- CRUD operations using preparedStatement --------->
+	        
+//	        <-- for insert --> 
+//	        String query = "INSERT INTO teacher(name, age, marks) VALUES(?, ?, ?)";
+//	        PreparedStatement ps = ((java.sql.Connection) connection).prepareStatement(query);
+//	        ps.setString(1, "Anshika");
+//	        ps.setInt(2, 21);
+//	        ps.setDouble(3, 93.45);
+	        
+	        
+//	        <-- for select -->
+//	        String query = "SELECT marks from teacher WHERE id = ?";	        
+//	        PreparedStatement ps = ((java.sql.Connection) connection).prepareStatement(query);
+//	        ps.setInt(1, 1);
+//	        
+//	        ResultSet rs = ps.executeQuery();
+			
+			
+//			<-- for update --> 
+//			String query = "UPDATE teacher SET marks = ? WHERE id = ?";	        
+//	        PreparedStatement ps = ((java.sql.Connection) connection).prepareStatement(query);
+//	        ps.setDouble(1, 98.80);
+//	        ps.setInt(2, 3);
+			
+			
+//			<-- for delete -->
+//			String query = "DELETE from teacher WHERE id = ?";	        
+//	        PreparedStatement ps = ((java.sql.Connection) connection).prepareStatement(query);
+//	        ps.setDouble(1, 98.80);
+//	        ps.setInt(1, 3);
+	        
+	        
+			
+//			< --- by using while loop and user input --->
+			String query = "INSERT INTO teacher(name, age, marks) VALUES(?, ?, ?)";
+			PreparedStatement ps = ((java.sql.Connection) connection).prepareStatement(query);
+			Scanner sc = new Scanner(System.in);
+			while(true) {
+				System.out.println("Enter your name: ");
+				String name = sc.next();
+				System.out.println("Enter your age: ");
+				int age = sc.nextInt();
+				System.out.println("Enter your marks: ");
+                double marks = sc.nextDouble();
+                String choice = sc.next();
+                ps.setString(1, name);
+                ps.setInt(2,age);
+                ps.setDouble(3, marks);
+                ps.addBatch();
+                if(choice.toUpperCase().equals("N")) {
+                	break;
+                }
+			}
+			
+			int arr[] = ps.executeBatch();
+			for(int i=0; i<arr.length; i++) {
+				if(arr[i]==0) {
+					System.out.println("Query "+i+" is not executed successfully ");
+				}
+			}
+			
+			
+//	        int rs = ps.executeUpdate();
+//	        if(rs>0) {
+//	        	System.out.println("Data Deleted Successfully");
+//	        }else {
+//	        	System.out.println("any error");
+//	        }
+
 			
 		} catch (SQLException e) {
 			// TODO: handle exception regards SQL 
